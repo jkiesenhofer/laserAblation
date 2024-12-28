f1b0664a4f0719dea6f3559138dc2c610f5472e0
natbox <179704619+natbox@users.noreply.github.com>
2024-12-28 17:47:51 +0100

    Update vofLaser.py

--- vofLaser.py
+++ vofLaser.py
@@ -97,7 +97,7 @@ for z in range(len(convexite)):
   vof[z]=abs(vof[z]-vof[z-1])
 #plt.xlim(0,50)
 #plt.ylim(0.0,2.5)
-plt.title("refractive velocity")
+plt.title("refractive index")
 plt.plot(vof)
 plt.grid()
 plt.show()
